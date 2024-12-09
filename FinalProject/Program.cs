30d54916b16ccebec1c7d141a8f5e37f4318615e
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2024-12-08 17:13:40 -0800

    Add project files.

---
+++ FinalProject/Program.cs
@@ -0,0 +1,24 @@
+﻿// Singleton Pattern
+// This project aims to demonstrate a creation of an instance and avoid creating other instances
+// This design would fail at having multiple threads and could possibly confuse itself, depending on if the program detects too much of all the threads
+// This design would be good at having a access point to that one instance and has only one instance
+
+using System;
+using FinalProject;
+using FinalProject.FinalProject;
+
+Thread t1 = new Thread(() =>
+{
+    var instance = Uploadservice.Instance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    var instance = Uploadservice.Instance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ FinalProject/UploadService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    namespace FinalProject
+    {
+        class Uploadservice
+        {
+            private Uploadservice() { }
+
+            public int id { get; private set; }
+
+            private static Uploadservice _instance;
+
+            private static readonly _lock = new object ()
+
+            public static Uploadservice Instance(int id)
+            {
+                if (_instance == null)
+                {
+
+                    lock (_lock)
+                    {
+                        if (_Instance == null)
+                        {
+                            _instance = new Uploadservice();
+                            _instance.id = id;
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
